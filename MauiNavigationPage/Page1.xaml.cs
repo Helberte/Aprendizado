f1cd29de46754467e5cabe73c686b66f728b402a
Helberte <helberteads@gmail.com>
2024-02-18 18:06:19 -0400

    adicionando paginas para navegação

---
+++ MauiNavigationPage/Page1.xaml.cs
@@ -0,0 +1,9 @@
+namespace MauiNavigationPage;
+
+public partial class Page1 : ContentPage
+{
+	public Page1()
+	{
+		InitializeComponent();
+	}
+}
---
+++ MauiNavigationPage/Page3.xaml.cs
@@ -0,0 +1,9 @@
+namespace MauiNavigationPage;
+
+public partial class Page3 : ContentPage
+{
+	public Page3()
+	{
+		InitializeComponent();
+	}
+}
