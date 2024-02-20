08f274de0116a4e4fd7d19cd8f72fe09d4f18020
Helberte <helberteads@gmail.com>
2024-02-19 22:04:53 -0400

    criando a navegação entre as páginas no navigationPage

--- MauiNavigationPage/Page1.xaml.cs
+++ MauiNavigationPage/Page1.xaml.cs
@@ -6,4 +6,10 @@ public partial class Page1 : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+		// propriedade só funciona se a pagina atual estiver configurada com a navigation page
+		Navigation.PushAsync(new Page2());
+    }
 }
