9f5ec8ce280d6a78019c15bede6c0fef8e7a8256
Helberte <helberteads@gmail.com>
2024-02-20 20:55:10 -0400

    conhecendo alguns metodos essenciais do navigationPage

--- MauiNavigationPage/Page2.xaml.cs
+++ MauiNavigationPage/Page2.xaml.cs
@@ -7,9 +7,28 @@ public partial class Page2 : ContentPage
 		InitializeComponent();
 	}
 
-    private void Button_Clicked(object sender, EventArgs e)
+    private void OnButtonClicked(object sender, EventArgs e)
     {
 		Navigation.PushAsync(new Page3());
     }
 
-}
+    private void OnButtonClickedVoltar(object sender, EventArgs e)
+    {
+        Navigation.PopAsync();
+
+        // permite saber a quantidade de páginas
+        _ = Navigation.NavigationStack.Count;
+
+        // também é possível obter a página em que quiser
+        _ = Navigation.NavigationStack[0];
+
+        // volta para o modal inicial
+        Navigation.PopToRootAsync();
+
+        // remove uma página especificada
+        Navigation.RemovePage(Navigation.NavigationStack[3]);
+
+        // é possível inserir uma página antes da página especificada
+        Navigation.InsertPageBefore(new Page(), Navigation.NavigationStack[1]);
+    }
+} 
