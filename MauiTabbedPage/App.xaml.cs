2043e6d19bc7b6601f3dc6dc09116903bc2c9ed4
Helberte <helberteads@gmail.com>
2024-02-25 18:02:38 -0400

    inicio tabbedpage

---
+++ MauiTabbedPage/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiTabbedPage
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
