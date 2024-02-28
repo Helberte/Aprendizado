2e4ff072f71dcb96b60cd9ba369cf15f4d2e3fbc
Helberte Costa <helberte.arruda@redeig.com>
2024-02-28 08:06:37 -0400

    Adiciona documentação e referencias ao criador

--- Graficos/MainActivity.cs
+++ Graficos/MainActivity.cs
@@ -5,28 +5,30 @@ using Android.Util;
 using Android.Views;
 using Android.Widget;
 using AndroidX.AppCompat.App;
-using AndroidX.CardView.Widget;
-using AndroidX.RecyclerView.Widget;
 using Microcharts;
 using Microcharts.Droid;
 using SkiaSharp;
 using System;
 using System.Collections.Generic;
-using System.Drawing;
-using static Android.Security.Identity.CredentialDataResult;
 
 namespace Graficos
 {
+    /// <summary>
+    /// Implementa lógica com gráficos bonitos.
+    /// Crédito: https://github.com/microcharts-dotnet/Microcharts
+    /// Wiki:    https://github.com/microcharts-dotnet/Microcharts/wiki
+    /// </summary>
+    /// <remarks>Helberte Costa, 27/02/2024</remarks>
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true, ScreenOrientation = Android.Content.PM.ScreenOrientation.Portrait)]
     public class MainActivity : AppCompatActivity
     {
         readonly List<ChartEntry> entries = new List<ChartEntry>();
 
-        private ChartView            chartView     = null;
-        private System.Timers.Timer  meuTimer      = null;
-        private HorizontalScrollView scrollView    = null;
-        private BarChart             barras;
-        private int                  valorAleatorio;
+        private ChartView chartView = null;
+        private System.Timers.Timer meuTimer = null;
+        private HorizontalScrollView scrollView = null;
+        private BarChart barras;
+        private int valorAleatorio;
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
@@ -34,19 +36,24 @@ namespace Graficos
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             SetContentView(Resource.Layout.activity_main);
 
-            chartView         = FindViewById<ChartView>            (Resource.Id.chartView);
-            scrollView        = FindViewById<HorizontalScrollView> (Resource.Id.scrollView);
+            // obtém as referências do layout
+            chartView = FindViewById<ChartView>(Resource.Id.chartView);
+            scrollView = FindViewById<HorizontalScrollView>(Resource.Id.scrollView);
 
-            meuTimer          = new System.Timers.Timer(1000);
+            // inicializa o timer
+            meuTimer = new System.Timers.Timer(1000);
             meuTimer.Elapsed += MeuTimer_Elapsed;
 
-            valorAleatorio    = new Random().Next(0, 1000);
+            // gera valor randomico
+            valorAleatorio = new Random().Next(500, 600);
+
+            // adiciona um item que será uma barra no gráfico
             entries.Add(
                 new ChartEntry(valorAleatorio)
                 {
-                    Label      = "UWP",
+                    Label = "-",
                     ValueLabel = valorAleatorio.ToString(),
-                    Color      = valorAleatorio < 500 ? SKColor.Parse("#FF6C6C") : SKColor.Parse("#D6D547")
+                    Color = valorAleatorio < 550 ? SKColor.Parse("#FF6C6C") : SKColor.Parse("#D6D547")
                 }
             );
 
@@ -54,43 +61,51 @@ namespace Graficos
             barras = new BarChart
             {
                 LabelOrientation = Microcharts.Orientation.Vertical,
-                IsAnimated       = true,
-                Entries          = entries,
-                Margin           = 10
+                IsAnimated = true,
+                Entries = entries,
+                Margin = 10,
+                BackgroundColor = SKColor.Parse("#FFFFFF")
             };
-            barras.IsAnimated    = false;
+            barras.IsAnimated = false;
 
+            // seta propriedade com o gráfico no elemento de tela
             chartView.Chart = barras;
 
+            // inicia o timer
             meuTimer.Start();
         }
 
         private void MeuTimer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
         {
+            // volta para a thread principal
             RunOnUiThread(() =>
-            {                
+            {
                 ViewGroup.LayoutParams layoutParams = chartView.LayoutParameters;
-                
-                valorAleatorio = new Random().Next(0, 1000);
+
+                valorAleatorio = new Random().Next(500, 600);
 
                 entries.Add(
                     new ChartEntry(valorAleatorio)
                     {
-                        Label      = "UWP",
+                        Label = "-",
                         ValueLabel = valorAleatorio.ToString(),
-                        Color      = valorAleatorio < 500 ? SKColor.Parse("#FF6C6C") : SKColor.Parse("#D6D547")
+                        Color = valorAleatorio < 550 ? SKColor.Parse("#FF6C6C") : SKColor.Parse("#D6D547")
                     }
                 );
 
+                // elimina as primeiras caso a quantidade de barras ultrapasse o limite
                 EliminaPrimeiras(entries, 50);
 
+                // converte valor em dip, em pixels para aumentar a largura do componente de tela
                 layoutParams.Width = (int)TypedValue.ApplyDimension(ComplexUnitType.Dip, entries.Count * 15, Application.Context.Resources.DisplayMetrics);
 
                 chartView.LayoutParameters = layoutParams;
 
-                chartView.Chart    = null;
-                chartView.Chart    = barras;
+                // reinicia a propriedade que contém o gráfico
+                chartView.Chart = null;
+                chartView.Chart = barras;
 
+                // move o scroll para o fim do horizontalscroll
                 scrollView.ScrollX = 16 * entries.Count;
             });
         }
@@ -110,6 +125,6 @@ namespace Graficos
 
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
         }
-              
+
     }
 }
